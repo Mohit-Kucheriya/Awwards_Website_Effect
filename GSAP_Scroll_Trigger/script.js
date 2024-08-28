7b62896d992da5b971d8872ef69a0836d459c9f6
Mohit Kucheriya <kucheriyamohit@gmail.com>
2024-08-28 21:58:32 +0530

    @import url(https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,700;0,800;0,900;1,900

---
+++ GSAP_Scroll_Trigger/script.js
@@ -0,0 +1,18 @@
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".part2",
+        start: "0% 90%",
+        end: "180% 90%",
+        scrub: true,
+        markers: true,
+
+    }
+})
+
+tl.to(".stripe-left", {
+    marginLeft: "0%"
+}, "text")
+tl.to(".stripe-right", {
+    marginLeft: "-50%"
+}, "text")
+
